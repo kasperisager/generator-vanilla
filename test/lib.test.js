88995cd8fd92966634e72eb1b90d3d4883b2e316
kasperisager <kasperisager@gmail.com>
2014-04-29 16:45:46 -0400

    Error handling in generator utility tests

--- test/lib.test.js
+++ test/lib.test.js
@@ -7,21 +7,25 @@ var path   = require('path')
 describe('generator utitilies', function () {
   describe('#getAddon()', function () {
     it('parses application meta into JSON', function (done) {
-      var match = require('./fixtures/application/settings/about.json')
-        , base  = path.resolve(__dirname, 'fixtures/application');
+      var fixture = require('./fixtures/application/settings/about.json')
+        , base    = path.resolve(__dirname, 'fixtures/application');
 
       utils.getAddon(base, 'Application', function (err, addon) {
-        addon.should.deep.equal(match);
+        if (err) return done(err);
+
+        addon.should.deep.equal(fixture);
         done();
       });
     });
 
     it('parses theme meta into JSON', function (done) {
-      var match = require('./fixtures/theme/about.json')
-        , base  = path.resolve(__dirname, 'fixtures/theme');
+      var fixture = require('./fixtures/theme/about.json')
+        , base    = path.resolve(__dirname, 'fixtures/theme');
 
       utils.getAddon(base, 'Theme', function (err, addon) {
-        addon.should.deep.equal(match);
+        if (err) return done(err);
+
+        addon.should.deep.equal(fixture);
         done();
       });
     });
